0985cb447b14ea01c01a31f0d01c234a969a53be
michaelfilho <michaelfilho175@gmail.com>
2025-06-30 16:32:34 -0300

    Alteração

--- marcacao/index.php
+++ marcacao/index.php
@@ -53,8 +53,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <title>TEMPUS - Marcação</title>
     <link rel="shortcut icon" href="../css/imagens/1.png" type="image/x-icon">
     <style>
-        @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap');
-
+    @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap');
         * {
             margin: 0;
             padding: 0;
@@ -280,8 +279,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             color: #27ae60;
             font-weight: bold;
         }
-
-        /* Responsivo */
         @media (max-width: 768px) {
             .info-box {
                 flex-direction: column;
